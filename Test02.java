617bb8d2c304ee0944c22a6265c8e78dcc9d2b20
Dzy-hue <diazprahadyan999@gmail.com>
2025-10-02 12:26:48 +0700

    Saya Mengubah 1

--- Test02.java
+++ Test02.java
@@ -2,5 +2,6 @@ public class Test02 {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+        System.out.println("Hi, saya Diaz ini baris kedua");
     }
 }
